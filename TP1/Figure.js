8cc07983da0111f8591fb26c680f46d54e202bb3
unknown <alexis.chevet@student.yncrea.fr>
2021-09-30 17:05:17 +0200

    ACT Ajout boutons, réduction du temps d'affichage, guide d'utilisation

--- TP1/Figure.js
+++ TP1/Figure.js
@@ -1,8 +1,8 @@
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / (window.innerHeight-200), 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / (window.innerHeight-250), 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight-200);
+renderer.setSize( window.innerWidth, window.innerHeight-250);
 document.body.appendChild( renderer.domElement ); //création de la scène 
 
 const material = new THREE.PointsMaterial({ //définition taille et couleurs des points
@@ -12,8 +12,47 @@ const material = new THREE.PointsMaterial({ //définition taille et couleurs des
 
 const points = []; // tableau de points
 
-let button = document.getElementById("valider");
-button.addEventListener("click", calculate);
+let button_reset=document.getElementById("reset");
+button_reset.addEventListener("click",reset);
+
+
+let button_valider = document.getElementById("valider");
+button_valider.addEventListener("click", calculate);
+
+let cercle=document.getElementById("cercle");
+let coeur=document.getElementById("coeur");
+
+cercle.addEventListener("click",drawCircle);
+coeur.addEventListener("click",drawHeart);
+
+/**
+ * Réinitialise le canvas
+ */
+function reset(){
+    document.location.reload(true);
+}
+
+/**
+ * Dessine un cercle avec l'équation paramétrique de l'exercice
+ */
+function drawCircle(){
+    let coordX=document.getElementById("X");
+    coordX.setAttribute('value',"2*cos(t)");
+    let coordY=document.getElementById("Y");
+    coordY.setAttribute('value',"2*sin(t)");
+    calculate();
+}
+
+/**
+ * Dessine un coeur avec l'équation paramétrique de l'exercice
+ */
+function drawHeart(){
+    let coord2X=document.getElementById("X");
+    coord2X.setAttribute('value',"pow(sin(t), 3)");
+    let coord2Y=document.getElementById("Y");
+    coord2Y.setAttribute('value',"cos(t)-pow(cos(t),4)"); 
+    calculate();
+}
 
 /**
  * Récupère les équations, calcule la courbe et affiche le rendu
@@ -24,7 +63,7 @@ function calculate(){
     x = transformString(X.value);
     y = transformString(Y.value);
 
-    for(let t = 0; t < 2*Math.PI; t+=0.0001){
+    for(let t = 0; t < 2*Math.PI; t+=0.001){    // Calcul de tous les points de la courbe
         points.push( new THREE.Vector3( evaluate(t,x), evaluate(t,y), 0 ) );
     }
     
@@ -45,12 +84,14 @@ function afficher(){
 }
 
 /**
- * Modifie une équation pour qu'elle soit lisible lors de l'évaluation. Elle ajoute la syntax de Math.js
+ * Modifie une équation pour qu'elle soit lisible lors de l'évaluation. Elle ajoute la syntaxe de Math.js
  * @param {string} str 
  * @returns {string}
  */
 function transformString(str){
-    let tab = str.split("cos");
+    let tab = str.split("tan");
+    str = tab.join("Math.tan");
+    tab = str.split("cos");
     str = tab.join("Math.cos");
     tab = str.split("sin");
     str = tab.join("Math.sin");
@@ -62,6 +103,8 @@ function transformString(str){
     str = tab.join("Math.pow");
     tab = str.split("exp");
     str = tab.join("Math.exp");
+    tab = str.split("ln");
+    str = tab.join("Math.log");
     return str;
 }
 
